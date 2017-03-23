0aaf2703517df7840dc9dbec240386f7f3c6520b
nanyr <nanyramirez0@gmail.com>
2017-03-23 17:42:13 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function theTruth(){
+  return true
+}
+
+function sayHey(){
+  return "Hey!"
+}
+
+function sayHeyFriend(name){
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(flavor){
+  if(flavor=='mint chocolate chip'){
+  return 'I love mint chocolate chip'}
+}
+
+function shouting(string){
+  return string.toUpperCase();
+}
+
+function roundDown(num){
+  return Math.floor(num)
+}
